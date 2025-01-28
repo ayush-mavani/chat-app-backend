ce941166d3e7542c3792d460d2c77929d2288977
ayush mavani <107191192+ayush-mavani@users.noreply.github.com>
2025-01-28 15:30:18 +0530

    fix issue

--- server.js
+++ server.js
@@ -12,57 +12,57 @@ app.get("/", async (req, res) => {
   return res.json({ status: true, message: "Chat App..." });
 });
 
-app.post("/room/:roomName/note", async (req, res) => {
-  try {
-    console.log("room node...", req.body, req.params);
+// app.post("/room/:roomName/note", async (req, res) => {
+//   try {
+//     console.log("room node...", req.body, req.params);
 
-    const { content } = req.body;
-    const { roomName } = req.params;
+//     const { content } = req.body;
+//     const { roomName } = req.params;
 
-    await Room.findOneAndUpdate({ roomName }, { content });
+//     await Room.findOneAndUpdate({ roomName }, { content });
 
-    return res.json({ status: true, message: "Note Updated!" });
-  } catch (err) {
-    console.log("rooom note error...", err);
-    return res.json({ status: false, message: "Server Error!" });
-  }
-});
+//     return res.json({ status: true, message: "Note Updated!" });
+//   } catch (err) {
+//     console.log("rooom note error...", err);
+//     return res.json({ status: false, message: "Server Error!" });
+//   }
+// });
 
-app.get("/deleteAllUser", async (req, res) => {
-  try {
-    await Room.deleteMany();
-    return res.json({ status: true, message: "Delete all User!" });
-  } catch (err) {
-    return res.json({ status: false, message: "Server Error!" });
-  }
-});
+// app.get("/deleteAllUser", async (req, res) => {
+//   try {
+//     await Room.deleteMany();
+//     return res.json({ status: true, message: "Delete all User!" });
+//   } catch (err) {
+//     return res.json({ status: false, message: "Server Error!" });
+//   }
+// });
 
-app.get("/userByRoom/:roomName", async (req, res) => {
-  try {
-    console.log("room...", req.params);
+// app.get("/userByRoom/:roomName", async (req, res) => {
+//   try {
+//     console.log("room...", req.params);
 
-    const { roomName } = req.params;
+//     const { roomName } = req.params;
 
-    let result = await Room.find({ roomName });
-    console.log("result::: ", result);
+//     let result = await Room.find({ roomName });
+//     console.log("result::: ", result);
 
-    return res.json({ status: true, message: "Room Created!", data: result });
-  } catch (err) {
-    console.log("room error...", err);
-    return res.json({ status: false, message: "Server Error!" });
-  }
-});
+//     return res.json({ status: true, message: "Room Created!", data: result });
+//   } catch (err) {
+//     console.log("room error...", err);
+//     return res.json({ status: false, message: "Server Error!" });
+//   }
+// });
 
-connect(env.DB_CONNECTION_STRING)
-  .then(async () => {
-    console.log("Database connected!");
-  })
-  .catch((err) => {
-    console.log("Error connecting...", err.message);
-  });
+// connect(env.DB_CONNECTION_STRING)
+//   .then(async () => {
+//     console.log("Database connected!");
+//   })
+//   .catch((err) => {
+//     console.log("Error connecting...", err.message);
+//   });
 
 const Http = require("http").createServer(app);
-const socket = require("./app/socket/socket");
-socket(Http);
+// const socket = require("./app/socket/socket");
+// socket(Http);
 
 module.exports = { Http };
