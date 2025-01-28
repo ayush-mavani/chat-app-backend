03255fa2b00fa9403d932dc53645699f3035176f
ayush mavani <107191192+ayush-mavani@users.noreply.github.com>
2025-01-28 15:17:06 +0530

    fixed node issue

--- index.js
+++ index.js
@@ -1,76 +1,8 @@
-// const express = require("express");
-// const app = express();
-// const cors = require("cors");
-// const connect = require("./app/config/database");
-// const env = require("./app/config/env");
-// const Room = require("./app/models/room");
+const env = require("./app/config/env");
+const { Http } = require("./server");
 
-// app.use(cors());
-// app.use(express.json());
+Http.listen(env.PORT, () => {
+  console.log("listening on port " + env.PORT);
+});
 
-// app.get("/", async (req, res) => {
-//   return res.json({ status: true, message: "Chat App..." });
-// });
-
-// app.post("/room/:roomName/note", async (req, res) => {
-//   try {
-//     console.log("room node...", req.body, req.params);
-
-//     const { content } = req.body;
-//     const { roomName } = req.params;
-
-//     await Room.findOneAndUpdate({ roomName }, { content });
-
-//     return res.json({ status: true, message: "Note Updated!" });
-//   } catch (err) {
-//     console.log("rooom note error...", err);
-//     return res.json({ status: false, message: "Server Error!" });
-//   }
-// });
-
-// app.get("/deleteAllUser", async (req, res) => {
-//   try {
-//     await Room.deleteMany();
-//     return res.json({ status: true, message: "Delete all User!" });
-//   } catch (err) {
-//     return res.json({ status: false, message: "Server Error!" });
-//   }
-// });
-
-// app.get("/userByRoom/:roomName", async (req, res) => {
-//   try {
-//     console.log("room...", req.params);
-
-//     const { roomName } = req.params;
-
-//     let result = await Room.find({ roomName });
-//     console.log("result::: ", result);
-
-//     return res.json({ status: true, message: "Room Created!", data: result });
-//   } catch (err) {
-//     console.log("room error...", err);
-//     return res.json({ status: false, message: "Server Error!" });
-//   }
-// });
-
-// connect(env.DB_CONNECTION_STRING)
-//   .then(async () => {
-//     console.log("Database connected!");
-//   })
-//   .catch((err) => {
-//     console.log("Error connecting...", err.message);
-//   });
-
-// const Http = require("http").createServer(app);
-// const socket = require("./app/socket/socket");
-// socket(Http);
-
-// Http.listen(env.PORT, () => {
-//   console.log("listening on port " + env.PORT);
-// });
-
-// module.exports = { Http };
-
-module.exports = (req, res) => {
-  res.status(200).send("Hello, Vercel!");
-};
+module.exports = { Http };
