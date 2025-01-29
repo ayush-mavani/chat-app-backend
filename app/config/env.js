a61feeee1e60dec0a6d0a961e5d4645f7122b924
ayush mavani <107191192+ayush-mavani@users.noreply.github.com>
2025-01-29 11:47:18 +0530

    change mongo db

--- app/config/env.js
+++ app/config/env.js
@@ -1,8 +1,6 @@
 require("dotenv/config");
 
 module.exports = {
-  NODE_ENV: process.env.NODE_ENV,
-  PRODUCT_NAME: process.env.PRODUCT_NAME,
   PORT: process.env.PORT,
   DB_CONNECTION_STRING: process.env.DB_CONNECTION_STRING,
 };
